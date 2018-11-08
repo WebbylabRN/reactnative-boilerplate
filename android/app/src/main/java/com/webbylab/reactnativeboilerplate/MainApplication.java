ca74c16f430e90d3a90c7093c397f2046157fd88
Vira Rodionova <bepa.rdnv@gmail.com>
2018-11-08 19:17:38 +0200

    install and link react-native-config

--- android/app/src/main/java/com/webbylab/reactnativeboilerplate/MainApplication.java
+++ android/app/src/main/java/com/webbylab/reactnativeboilerplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.webbylab.reactnativeboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new ReactNativeConfigPackage()
       );
     }
 
