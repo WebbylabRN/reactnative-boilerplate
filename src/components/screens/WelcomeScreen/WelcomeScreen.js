bfe59d7f31c5f255f4f467bf84780f2d0f2aac47
Vira Rodionova <bepa.rdnv@gmail.com>
2018-11-19 16:13:25 +0200

    move screens

--- src/components/screens/WelcomeScreen/WelcomeScreen.js
+++ src/components/screens/WelcomeScreen/WelcomeScreen.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes            from 'prop-types';
 import { View, Text }       from 'react-native';
 
 import styles from './WelcomeScreenStyles.js';
 
 class WelcomeScreen extends Component {
+    static propTypes = {};
+
+    static defaultProps = {};
+
     render() {
         return (
             <View style={styles.layout}>
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,6 +1,6 @@
 import { Navigation } from 'react-native-navigation';
 
-import WelcomeScreen from '../screens/WelcomeScreen/WelcomeScreen.js';
+import WelcomeScreen from '../components/screens/WelcomeScreen/WelcomeScreen.js';
 
 export function registerComponents() {
     Navigation.registerComponent('navigation.WelcomeScreen', () => WelcomeScreen);
