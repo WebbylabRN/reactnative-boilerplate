08cde3204a1efe0b8d04e34e89dc39f491870dcb
Vira Rodionova <bepa.rdnv@gmail.com>
2018-11-21 12:05:36 +0200

    vector icons usage

--- src/components/screens/WelcomeScreen/WelcomeScreen.js
+++ src/components/screens/WelcomeScreen/WelcomeScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes            from 'prop-types';
 import { View, Text }       from 'react-native';
+import Icon                 from 'react-native-vector-icons/FontAwesome';
+
+import colors    from '../../../constants/colors.js';
+import { isIOS } from '../../../utils/platformUtils.js';
 
 import styles from './WelcomeScreenStyles.js';
 
@@ -10,9 +13,12 @@ class WelcomeScreen extends Component {
     static defaultProps = {};
 
     render() {
+        const text = 'Welcome To Boilerplate';
+
         return (
             <View style={styles.layout}>
-                <Text>Welcome To Boilerplate</Text>
+                <Icon name={isIOS ? 'apple' : 'android'} size={50} color={colors.PRIMARY_MAIN} />
+                <Text>{text}</Text>
             </View>
         );
     }
